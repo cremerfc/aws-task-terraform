55e24960894aa592fc3077f0fe2f852dae53e722
cremerfc <cremerfc@users.noreply.github.com>
2020-03-10 15:41:29 -0400

    Update main.tf

--- workspace/main.tf
+++ workspace/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "terraform"
+}
+
 resource "aws_ecs_task_definition" "service" {
   family                = "service"
   container_definitions = "${file("../task-definitions/service.json")}"
