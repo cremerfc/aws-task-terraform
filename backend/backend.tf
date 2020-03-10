0b186ed03badb5a69d5a46723483e1512b9e05ab
cremerfc <cremerfc@users.noreply.github.com>
2020-03-10 15:17:46 -0400

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,14 +1,6 @@
-resource "aws_ecs_task_definition" "service" {
-  family                = "service"
-  container_definitions = "${file("task-definitions/service.json")}"
-
-  volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
-}
+    
+bucket  = "armory-se-terraformer-demo"
+key     = "demo-api-dev.tfstate"
+region  = "us-east-1"
+encrypt = false
+profile = "terraform"
